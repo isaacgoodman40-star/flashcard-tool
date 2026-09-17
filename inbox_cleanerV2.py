d1a6683f9084cf963523741ff4f796e43f3e7965
isaacgoodman40-star <isaacgoodman40@gmail.com>
2026-09-17 17:54:00 +0100

    Create inbox_cleanerV2.py

---
+++ inbox_cleanerV2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="Inbox Cleaner",
+)
+
+st.title("Inbox Cleaner")
+st.write("A privacy-first way to clean your inbox.")
+st.success("The application is running.")
