9d5221b93f6679733419c0932be025291dd0eabf
isaacgoodman40-star <isaacgoodman40@gmail.com>
2026-09-20 08:55:48 +0100

    Update inbox_cleanerV2.py

--- inbox_cleanerV2.py
+++ inbox_cleanerV2.py
@@ -35,7 +35,7 @@ st.subheader("Connect your mailbox")
 st.write("connect your email account securely using your provider's sign-in page.")
 
 if not st.user.is_logged_in:
-        st.warning("Gmail connection is temporarily disabled" 
+        st.warning("Gmail connection is temporarily disabled " 
                    "While we finish the secure authorisation process.")
 
 else:
