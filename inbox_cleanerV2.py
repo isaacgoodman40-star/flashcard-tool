fe7fd72d870636fba4ef281c3532ac01587c0601
isaacgoodman40-star <isaacgoodman40@gmail.com>
2026-09-20 08:42:21 +0100

    Update inbox_cleanerV2.py

--- inbox_cleanerV2.py
+++ inbox_cleanerV2.py
@@ -4,7 +4,7 @@ from google_auth_oauthlib.flow import Flow
 GMAIL_SCOPES = ["https://www.googleapis.com/auth/gmail.metadata"]
 
 def create_gmail_flow():
-    google_config = st.secrets["auth"]["google"]
+    google_config = st.secrets["google_oauth"]
 
     client_config = {
         "web": {
